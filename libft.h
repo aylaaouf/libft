2ca9e06690033221fa376aa5e76c7ce07ae7cde1
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-08-23 21:36:46 +0100

    add more func

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aylaaoufi <ayoub.laaoufi@hotmail.com>      +#+  +:+       +#+        */
+/*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 16:58:21 by aylaaoufi         #+#    #+#             */
-/*   Updated: 2024/08/22 17:52:47 by aylaaoufi        ###   ########.fr       */
+/*   Updated: 2024/08/23 21:28:30 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,12 @@
 
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
+int     ft_tolower(int c);
+int     ft_strncmp(const char *s1, const char *s2, size_t n);
+char    *ft_strcpy(char *dst, const char *src);
+int     ft_strcmp(const char *s1, const char *s2);
+int     ft_isdigit(int c);
+int     ft_isalpha(int c);
+int     ft_atoi(const char *str);
 
 #endif
