4e295706add31b4f3bd07f532c85a3b57a2a2ea1
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-08-24 00:44:53 +0100

    strncpy function

---
+++ ft_strncpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 00:25:33 by aylaaouf          #+#    #+#             */
+/*   Updated: 2024/08/24 00:43:48 by aylaaouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strncpy(char *dst, const char *src, size_t len)
+{
+    size_t i;
+
+    i = 0;
+    while (src[i] && i < len)
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    while (i < len)
+    {
+        dst[i] = '\0';
+        i++;
+    }
+    return (dst);
+}
