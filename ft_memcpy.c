ca63522bba8a957ad8d4e2bd20016abb772683b9
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-08-24 04:04:04 +0100

    func that copy src to dst by bite

---
+++ ft_memcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 03:18:41 by aylaaouf          #+#    #+#             */
+/*   Updated: 2024/08/24 04:02:47 by aylaaouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memcpy(void *dst, const void *src, size_t n)
+{
+    size_t i;
+    unsigned char *s1;
+    const unsigned char *s2;
+
+    s1 = (unsigned char *)dst;
+    s2 = (const unsigned char *)src;
+    if (!s1 && !s2)
+    {
+        return (NULL);
+    }
+    i = 0;
+    while (i < n)
+    {
+        s1[i] = s2[i];
+        i++;
+    }
+    return (dst);
+}
