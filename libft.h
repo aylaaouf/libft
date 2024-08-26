5fb3b6f6a78bbe76a61bcb9ca4d5852c7be9a271
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-08-26 02:21:03 +0100

    header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 16:58:21 by aylaaoufi         #+#    #+#             */
-/*   Updated: 2024/08/26 01:10:43 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2024/08/26 02:20:44 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ char    *ft_strncat(char *s1, const char *s2, size_t n);
 char    *ft_strncpy(char *dst, const char *src, size_t len);
 void    *ft_memcpy(void *dst, const void *src, size_t n);
 size_t  strlcpy(char *dst, const char *src, size_t size);
+void    ft_bzero(void *s, size_t len);
 
 #endif
