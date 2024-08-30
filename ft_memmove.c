1e8c64d687172481d213c21304dd62c80b65566f
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-08-30 01:49:14 +0100

    add more cases

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 08:04:34 by aylaaouf          #+#    #+#             */
-/*   Updated: 2024/08/27 08:32:23 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2024/08/29 02:30:38 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void *ft_memmove(void *dst, const void *src, size_t len)
     s1 = (unsigned char *)dst;
     s2 = (const unsigned char *)src;
     i = 0;
-    if (s1 > s2)
+    if (s1 > s2 && s1 < s2 + len)
     {
         while (len > 0)
         {
